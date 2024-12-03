5d4eeb64cfb7f593e2ea85a1360f2fa46a4c81a7
William0389 <c14220049@john.petra.ac.id>
2024-12-03 19:03:19 +0700

    fix: gradle version not compatible

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.application.room"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.application.room"
-        minSdk = 31
-        targetSdk = 34
+        minSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
